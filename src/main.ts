3ffdeabcdba8f30fe7ff6d3affefbb5228f89de6
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2020-10-13 08:19:59 +0530

    Add comments

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)//booting the angular app
   .catch(err => console.error(err));
